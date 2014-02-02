7e7620a5ad8cec7395ad0fd75df99f38a3d2f7f0
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-02-02 07:17:38 -0800

    Added explore_words method

--- word_chains.rb
+++ word_chains.rb
@@ -1,18 +1,44 @@
-def adjacent_words(current_word, dictionary)
-  dictionary.keep_if do |dict_word|
-    next unless dict_word.length == current_word.length
+def adjacent_words(root_word, dictionary)
+  dictionary.dup.tap do |selective_dictionary|
 
-    letters_off = 0
-    dict_word.split(//).each_with_index do |letter, index|
-      letters_off += 1 unless current_word[index] == letter
+    selective_dictionary.keep_if do |dict_word|
+      letters_off = 0
+      dict_word.split(//).each_with_index do |letter, index|
+        letters_off += 1 unless root_word[index] == letter
+      end
+      letters_off == 1 ? true : false
     end
-    letters_off == 1 ? true : false
+  end
+end
+
+def same_length(word, dictionary)
+  dictionary.keep_if { |dict_word| word.length == dict_word.length }
+end
+
+def explore_words(source, dictionary)
+  words_to_expand = [source]
+  all_reachable_words = [source]
+  candidate_words = same_length(source, dictionary)
+  p "EXPAND_LENGTH = 1"
+  p "REACH Length = 1"
+  p "candidates = #{candidate_words.length}"
+
+  until words_to_expand.empty?
+    current_word = words_to_expand.shift
+    current_word_children = adjacent_words(current_word, candidate_words)
+    current_word_children.each do |child_word|
+      words_to_expand << child_word
+      all_reachable_words << child_word
+      candidate_words.delete(child_word)
+    end
   end
 
-  dictionary
+  p "reachable #{all_reachable_words.length}"
+  p "candidates #{candidate_words.length}"
+  all_reachable_words
 end
 
 dict = File.readlines('dictionary.txt')
 dict.map! { |word| word.chomp }
 
-p adjacent_words("house", dict)
+explore_words("duck", dict)
